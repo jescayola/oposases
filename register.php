c883a63e0b47243f6e2bd285ca78d0676a91ad93
mnuez <mnuez@techberry.es>
2017-11-10 10:05:16 +0100

    registro
    v1 register y register_ok

---
+++ register.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+$userbbdd="mnuez";
+$passwordbbdd="ases!2017";
+$hostbbdd="techberry.es";
+$bbdd="opos";
+
+$conector=mysqli_connect($hostbbdd,$userbbdd,$passwordbbdd,$bbdd);
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>    </title>
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body
+	<div id="cajonaco">
+		<h1>REGÍSTRATE</h1><br/><br/><br/><br/>
+		<?php header("Content-Type: text/html;charset=utf-8"); ?>
+		<form action="register_ok.php" method="POST">
+			Nombre: <input type="text" name="name"/><br/>
+			Primer Apellido: <input type="text" name="apell1"/><br/>
+			Segundo Apellido: <input type="text" name="apell2"/><br/>
+			DNI: <input type="text" name="dni"/><br/>
+			Ciudad: <input type="text" name="ciudad"/><br/>
+			Teléfono: <input type="text" name="telefono"/><br/>
+			Email: <input type="text" name="email"/><br/>
+			Contraseña: <input type="password" name="pass1"/><br/>
+			Repite tu contraseña: <input type="password" name="pass2"/><br/>
+			
+			<input type="submit" name="submit" value="REGISTRAR"/>
+		</form>
+		<br/>
+		<br/>
+		<a href="index.php">HOME !!</a><br/>
+	
+	</div>
+</body>
+</html>
---
+++ register_ok.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+
+$userbbdd="mnuez";
+$passwordbbdd="ases!2017";
+$hostbbdd="techberry.es";
+$bbdd="opos";
+
+$conector=mysqli_connect($hostbbdd,$userbbdd,$passwordbbdd,$bbdd);
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>    </title>
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+	<div>
+		<?php
+		if (isset($_POST["submit"]))
+		{
+			if(empty($_POST["name"]) || empty($_POST["apell1"]) || empty($_POST["apell2"]) || empty($_POST["dni"]) ||   empty($_POST["ciudad"]) ||
+			empty($_POST["telefono"])  ||  empty($_POST["email"])  ||  empty($_POST["pass1"])  ||  empty($_POST["pass2"]))
+			{
+				echo "Rellena todos los campos porfavor <br/><br/><br/><a href='register.php'>Regístrate!</a>";
+			}
+			else
+			{
+				if($_POST["pass1"]==$_POST["pass2"])
+				{
+					mysqli_select_db($conector,"opos");
+					$num_filas=0;
+					$pass=$_POST["pass1"];
+					$name=$_POST["name"];
+					$consultasiyaexiste = mysqli_query($conector, "select * from USUARIOS where password='".$pass1."' AND nombre='".$name."'");
+					$num_filas = mysqli_num_rows($consultasiyaexiste);
+					if($num_filas>0)
+					{
+						echo "Ya existe el usuario!<p>Ya estás registrado!</p><br/><br/><br/>";
+						echo "<a href='register.php'>VUELVE A INTENTAR EL REGISTRO</a>";
+					}
+					else
+					{
+						$consulta="INSERT INTO USUARIOS (nombre, apellido1, apellido2, dni, ciudad, telefono, email, password) VALUES('".$_POST["name"]."','".$_POST["apell1"]."','".$_POST["apell2"]."','".$_POST["dni"]."','".$_POST["ciudad"]."','".$_POST["telefono"]."','".$_POST["email"]."','".$_POST["pass1"]."')";
+						mysqli_query($conector,$consulta);
+						echo "<h1>Ya estás registrado!!</h1><br/><br/><br/><br/>";
+					}
+				mysqli_close($conector);
+				}
+					else
+					{
+						echo "<p>Las contraseñas no coinciden!</p><br/><br/><br/>";
+						echo "<a href='register.php'>VUELVE A INTENTAR EL REGISTRO</a>";
+					}
+			}
+		}
+		else
+		{
+			header("location: index.php");
+		}
+		?>
+	</div>
+</body>
+</html>
