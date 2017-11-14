0363a9250e6310fee6be43a08847d9265bf265f9
mnuez <mnuez@techberry.es>
2017-11-14 10:01:31 +0100

    include
    conexionphp y plantilla

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+$userbbdd="mnuez";
+$passwordbbdd="ases!2017";
+$hostbbdd="techberry.es";
+$bbdd="opos";
+
+$conector=mysqli_connect($hostbbdd,$userbbdd,$passwordbbdd,$bbdd);
+?>
