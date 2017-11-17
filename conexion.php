051a3df3418512d9c9a03d423135df485766aee7
mnuez <mnuez@techberry.es>
2017-11-17 10:48:24 +0100

    sesiones
    conex

--- conexion.php
+++ conexion.php
@@ -1,10 +1,30 @@
 <?php
-session_start();
-
 $userbbdd="mnuez";
 $passwordbbdd="ases!2017";
 $hostbbdd="techberry.es";
 $bbdd="opos";
 
 $conector=mysqli_connect($hostbbdd,$userbbdd,$passwordbbdd,$bbdd);
+session_start();
+if(!isset($_SESSION["rol"]))
+{
+	echo "Este no es tu sitio <a href='login.php'> Vuelve a intentarlo </a>";
+	echo "<a href='login.php'> Volver al registro. </a>";
+	echo "<a href='index.php'> Volver al inicio. </a>";
+}
+else
+{
+	if($_SESSION["rol"]==1)
+	{
+		//admin
+	}
+	elseif($_SESSION["rol"]==2)
+	{
+		//profesor
+	}
+	elseif($_SESSION["rol"]==1)
+	{
+		//alumno
+	}
+}
 ?>
